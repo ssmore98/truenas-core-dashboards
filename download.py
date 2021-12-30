255d1f0278126d18ed8307dc31633ddad7acda40
Sachin More <sachinsureshmore@gmail.com>
2021-12-30 16:32:38 -0500

    first commit

---
+++ download.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import os
+import requests
+import glob
+import json
+import shutil
+from pprint import pprint
+
+server = "10.1.1.222:3000"
+GDIR   = '{HOME}/.config/grafana'.format(HOME=os.getenv("HOME"))
+
+uids = ['novl4l-ik']
+
+with open("{0}/token".format(GDIR), "r") as fp:
+    token = fp.read()
+    token = token.strip()
+
+grafana_headers = {"Authorization": "Bearer {0}".format(token),
+    "Content-Type":"application/json"}
+
+for uid in uids:
+    url = "http://{0}".format(server)
+    url = "{0}/api/dashboards/uid/{1}".format(url, uid)
+    with requests.get(url, headers=grafana_headers, verify=False) as response:
+        if response.ok:
+            retval = response.json()
+            filename = '{0}.json'.format(retval['dashboard']['title'])
+            print('Saving {0}'.format(filename))
+            with open(filename, "w") as fp:
+                json.dump(retval['dashboard'], fp, indent=4)
+            print('Saved {0}'.format(filename))
+        else:
+            print('Could not download dashboard {0}'.format(uid))
